83c164feaf6a6a398b740bbf70b7db68ad768d0f
Gerald Manipon <geraldjohn.m.manipon@jpl.nasa.gov>
2012-09-27 15:14:28 -0700

    moved scripts and configuration to ldos package

--- scripts/clean_queues.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo service rabbitmq-server stop
-sudo rm -rf /var/lib/rabbitmq/mnesia/*
-sudo service rabbitmq-server start
--- scripts/monitor_queues.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-clear
-status=`sudo rabbitmqctl list_queues`
-while [ 1 ]; do
-  echo "$status"
-  sleep 1
-  status=`sudo rabbitmqctl list_queues`
-  clear
-done
